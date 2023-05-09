1aa2550f5e05c4161fad5e47e9084ab57aac1f2a
Tyler J Chrestoff <datummine@proton.me>
2023-05-09 16:04:07 -0400

    Add User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_08_151624) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_09_200231) do
   create_table "experience_details", force: :cascade do |t|
     t.text "description"
     t.integer "experience_id", null: false
@@ -25,6 +25,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_08_151624) do
     t.date "start_date"
     t.date "end_date"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
